b67e096c9f2d57628b3aa9e0fa50f00770cff64c
rebertionfire <joaoreberti@gmail.com>
2019-12-07 11:18:57 +0000

    videos

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -30,3 +30,13 @@ function exitPopUp() {
     document.getElementById('popup').style.display = 'none'
     document.getElementById('opacity').style.display='none'
 }
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
