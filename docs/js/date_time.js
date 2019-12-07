303ad740392223ea3f64efacc9f4f1810b8a107a
GuiAndrew <gfilipea001@gmail.com>
2019-12-07 13:25:02 +0000

    remove alert

--- docs/js/date_time.js
+++ docs/js/date_time.js
@@ -6,7 +6,6 @@ setInterval(() => {
     const dateTime = new Date();
     let time = dateTime.toLocaleTimeString(locale);
     document.querySelector("#hour").innerHTML = time;
-    //alert('j');
 }, 1000);
 
 document.querySelector("#date").innerHTML = date.toLocaleDateString(locale, {
