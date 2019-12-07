7c8c3dbdb48ea9eebc645e4b2eb9fb3ed4f9978b
rebertionfire <joaoreberti@gmail.com>
2019-12-07 16:10:48 +0000

    things

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -34,16 +34,13 @@ function exitPopUp() {
 
 
 
-var mymap = L.map('mapid').setView([38.570571, -7.908909], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmViZXJ0aW9uZmlyZSIsImEiOiJjazN2amV1MXAwbWZnM2twaWcwZ3QwaWNqIn0.o9O9BaZyH0BuTm3dJZN4sg', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoicmViZXJ0aW9uZmlyZSIsImEiOiJjazN2amV1MXAwbWZnM2twaWcwZ3QwaWNqIn0.o9O9BaZyH0BuTm3dJZN4sg'
-}).addTo(mymap);
 
-L.marker([38.572758, -7.907294]).addTo(mymap)
-    .bindPopup('Templo Romano <br> Évora.')
+if (window.location.hash === '#text_modal_temple_diana'){
+
+    $('#text_modal_temple_diana').modal('show');
+}
+
+
 
 
