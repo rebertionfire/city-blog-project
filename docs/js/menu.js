b68272533dd804385b64329d086a56a3ca5bd40a
rebertionfire <joaoreberti@gmail.com>
2019-12-07 14:21:09 +0000

    fonts

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -31,12 +31,19 @@ function exitPopUp() {
     document.getElementById('opacity').style.display='none'
 }
 
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+
+var mymap = L.map('mapid').setView([38.570571, -7.908909], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmViZXJ0aW9uZmlyZSIsImEiOiJjazN2amV1MXAwbWZnM2twaWcwZ3QwaWNqIn0.o9O9BaZyH0BuTm3dJZN4sg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoicmViZXJ0aW9uZmlyZSIsImEiOiJjazN2amV1MXAwbWZnM2twaWcwZ3QwaWNqIn0.o9O9BaZyH0BuTm3dJZN4sg'
+}).addTo(mymap);
+
+L.marker([38.572758, -7.907294]).addTo(mymap)
+    .bindPopup('Templo Romano <br> Évora.')
+
+
